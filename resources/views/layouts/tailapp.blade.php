6e1dce4886e30190b38a8cb7b098bfecddc1e8d5
urio21 <hamphreyelisante@gamil.com>
2023-10-26 02:50:15 -0700

    reset password modal

--- resources/views/layouts/tailapp.blade.php
+++ resources/views/layouts/tailapp.blade.php
@@ -44,11 +44,30 @@
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
                             </li>
                             <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                            </li>
-                            <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-                            </li>
+                                <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" class="flex items-center justify-between w-full px-4 py-2 hover-bg-gray-100 dark-hover-bg-gray-600 dark-hover-text-white text-sm text-gray-700" id="settingsButton">
+                                  Settings
+                                  <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                  </svg>
+                                </button>
+                                <div id="doubleDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark-bg-gray-700">
+                                  <ul class="py-2 text-sm text-gray-700 dark-text-gray-200" aria-labelledby="doubleDropdownButton">
+                                    <li>
+                                        {{-- <div data-modal-toggle="user-modal" class="flex items-start justify-between p-5 border-b rounded-t">
+                                            <h3 class="text-xl font-semibold">
+                                               reset password
+                                            </h3>
+                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                            </button>
+                                        </div> --}}
+                                      <a data-modal-toggle="user-modal" href="#" class="block px-4 py-2 hover-bg-gray-100 dark-hover-bg-gray-600 dark-hover-text-white" id="resetPasswordButton">Reset Password</a>
+                                    </li>
+                                  </ul>
+                                </div>
+                              </li>
+
+
                             <li>
                                 <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                                     onclick="event.preventDefault();
